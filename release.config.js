6e45b1e0b41bb4e8d5a5c07fb26dedf596db4de9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-09-21 14:29:55 -0500

    Adding semantic-release

--- release.config.js
+++ release.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/Daniel-Benzion/gha-course-react-app",
-    plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator", 
-    "@semantic-release/npm", 
-    "@semantic-release/github"
-    ]
-
-}
+	branches: "main",
+	repositoryUrl: "https://github.com/Daniel-Benzion/gha-course-react-app",
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/npm",
+		"@semantic-release/github",
+	],
+};
